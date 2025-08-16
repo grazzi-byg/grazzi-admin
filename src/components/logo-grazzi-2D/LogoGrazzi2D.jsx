d9028aea4343374f20339bbbb61cc4db7c3ecc5b
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2025-08-16 15:17:59 -0500

    Login implemented

--- src/components/logo-grazzi-2D/LogoGrazzi2D.jsx
+++ src/components/logo-grazzi-2D/LogoGrazzi2D.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router";
 import "./LogoGrazzi2D.css";
 
 export default function LogoGrazzi2D() {
+  const navigate = useNavigate();
+  
   return (
-    <div className="logo-container" onClick={() => window.location.href = "/"}>
+    <div className="logo-container" onClick={() => navigate("/dashboard")}>
       <img src={"/images/logo/logo-grazzi-2d.svg"} alt="Grazzi" />
     </div>
   );
